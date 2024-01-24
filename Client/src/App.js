af024e971f197a6dfb465387c6b2f66469067c9e
tirumalarowthu <tirumalarowthuv@gmail.com>
2024-01-24 12:50:14 +0530

    forgot password feature added

--- Client/src/App.js
+++ Client/src/App.js
@@ -21,6 +21,9 @@ import AddQuestions from "./AddQuestions";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import PendingApprovals from "./PendingApprovals";
+import ForgotPassword from "./ForgotPassword";
+import VerifyOTP from "./VerifyOTP";
+import ResetPassword from "./ResetPassword";
 export const store = createContext();
 
 function App() {
@@ -35,6 +38,9 @@ function App() {
             <Route path="/" element={<Home/>} />
             <Route path="/Login" element={<LogIn/>} />
             <Route path="/login" element={<EvaluatorForm />} />
+            <Route path ="/forgot-password" element ={<ForgotPassword/>} />
+            <Route path="/verify-otp" element={<VerifyOTP/>}/>
+            <Route path="/reset-password" element ={<ResetPassword/>}/>
             <Route path="/myprofiledashboard" element={<Myprofile />} />
             <Route path="/pendingApprovals" element={<PendingApprovals />} />
             <Route path="/CandidateForm" element={<CandidateForm />} />
